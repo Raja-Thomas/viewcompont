efa9fd3afde470ce223b2fb13048ca28c451ea66
Raja-Thomas <60168092+Raja-Thomas@users.noreply.github.com>
2020-01-22 15:25:42 +0530

    Add project files.

---
+++ viewcomponents/EmployeeList1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using viewcompont.Models;
+namespace viewcompont.viewcomponents
+{
+    public class EmployeeList1 : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int noOfEmployee)
+        {
+            List<Employee> items = new List<Employee>();
+            for (var i = 0; i <= noOfEmployee; i++)
+            {
+                items.Add(new Employee { Id = i, Name = "Emp " + i.ToString() });
+            }
+            if (noOfEmployee > 5)
+            {
+                return View("EmployeeList1", items);
+            }
+            return View(items);
+        }
+    }
+}
